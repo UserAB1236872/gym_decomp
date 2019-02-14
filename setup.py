35fb71710f5b2974882399d774ee4e98651dabe0
Zaerei <Jragonmiris@gmail.com>
2019-02-13 18:40:33 -0800

    More setup tinkering

--- setup.py
+++ setup.py
@@ -8,16 +8,11 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from setuptools.command.develop import develop
 
-packages = find_packages(exclude=['tests'])
-base_dir = pathlib.Path(__file__).parent
-
-install_requires = ['pipenv']
-
-requirements = convert_deps_to_pip(pfile['packages'], r=False)
-requirements.append('pipenv')
-
-test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
-test_requirements.append('pipenv')
+requirements = ["numpy>=1.0",
+                "gym=*",
+                "hip-mdp-public=https://github.com/Zaerei/hip-mdp-public",
+                "llvmlite==0.27.0"
+                "numba==0.42.0"]
 
 with open(os.path.join(str(base_dir), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
