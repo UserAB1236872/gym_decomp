4a85972384924f5c8408a46be3ab2ee177ff5cfc
Zaerei <Jragonmiris@gmail.com>
2019-02-13 18:36:10 -0800

    Update setup

--- setup.py
+++ setup.py
@@ -8,30 +8,16 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from setuptools.command.develop import develop
 
-__requires__ = ['pipenv']
-
 packages = find_packages(exclude=['tests'])
 base_dir = pathlib.Path(__file__).parent
 
-pipenv_command = ['pipenv', 'install', '--deploy']
-pipenv_command_dev = ['pipenv', 'install', '--dev', '--deploy']
+install_requires = ['pipenv']
 
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+requirements.append('pipenv')
 
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-
-    def run(self):
-        subprocess.check_call(pipenv_command_dev)
-        develop.run(self)
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-
-    def run(self):
-        subprocess.check_call(pipenv_command)
-        install.run(self)
-
+test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+test_requirements.append('pipenv')
 
 with open(os.path.join(str(base_dir), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -42,8 +28,4 @@ setup(
     long_description='\n' + long_description,
     packages=packages,
     setup_requires=['setuptools_scm'],
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
 )
