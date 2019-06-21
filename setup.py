12a7a684d6b1036238a63a5d02446c402b52e595
Zoe Juozapaitis <jragonmiris@gmail.com>
2019-06-21 00:34:45 -0700

    Fix setup requirements

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@
 
 import os
 import pathlib
-import subprocess
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from setuptools.command.install import install
 from setuptools.command.develop import develop
 
-packages = find_packages(exclude=['tests'])
+packages = find_namespace_packages(exclude=['tests'])
 base_dir = pathlib.Path(__file__).parent
 
 requirements = ["numpy>=1.0",
-                "gym=*",
-                "hip-mdp-public=https://github.com/Zaerei/hip-mdp-public",
-                "llvmlite==0.27.0"
+                "gym==0.12.5",
+                "hip-mdp-public @ https://github.com/Zaerei/hip-mdp-public/archive/master.zip",
+                "llvmlite==0.27.0",
                 "numba==0.42.0"]
 
 with open(os.path.join(str(base_dir), 'README.md'), encoding='utf-8') as f:
@@ -25,5 +24,13 @@ setup(
     use_scm_version=False,
     long_description='\n' + long_description,
     packages=packages,
+    install_requires=requirements,
     setup_requires=['setuptools_scm'],
+    extras_require={
+        'dev': [
+            'pytest>=4.0',
+            'pylint>=2',
+            'autopep8>=0'
+        ]
+    }
 )
